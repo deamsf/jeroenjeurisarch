15c5b3c60b730701ec5456f7af38a133232f9c59
deamsf <frankdamen@gmail.com>
2025-01-21 16:50:13 +0100

    Update testimonials.ts

--- src/utils/testimonials.ts
+++ src/utils/testimonials.ts
@@ -8,16 +8,21 @@ export const testimonials: Testimonial[] = [
   {
     name: 'Frank D',
     role: 'Bouwheer',
-    quote: 'Jeroens moderne stijl sprak ons meteen aan.',
+    quote: 'Jeroens moderne stijl sprak ons meteen aan. Hij denkt mee en is actief betrokken in het project.',
   },
   {
-    name: 'Michael Chen',
-    role: 'Business Owner',
-    quote: 'Working with this studio was a game-changer for our office space. They created an environment that inspires creativity and productivity.',
+    name: 'Gregory F',
+    role: 'Bouwheer',
+    quote: 'Ik kan Jeroen alleen maar ten zeerste aanbevelen. Zeer vriendelijke en professionele architect die zijn vak zeer goed kent.',
   },
   {
-    name: 'Emma Rodriguez',
-    role: 'Interior Designer',
-    quote: 'Their architectural vision and ability to blend form with function is exceptional. Every project is a masterpiece of modern design.',
+    name: 'Koen R',
+    role: 'Bouwheer',
+    quote: 'Heel duidelijk en interessant gesprek gehad omtrent onze mogelijke verbouwing. Jeroen denkt mee na en zit op dezelfde golflengte!',
+  },
+  {
+    name: 'Peter',
+    role: 'Bouwheer',
+    quote: 'Jeroen durft meedenken en duwt niet zijn eigen visie door. Aangenaam om mee samen te werken!',
   },
 ];
